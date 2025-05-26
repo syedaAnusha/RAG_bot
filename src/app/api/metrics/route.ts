54b8597a15afc2be8e3cafe237310e7affc66768
Anusha Syeda <anoshasyeda1999@gmail.com>
2025-05-26 14:50:25 +0500

    rem multiple files

--- src/app/api/metrics/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-import { getMetrics } from "@/utils/monitoring";
-
-export async function GET() {
-  const metrics = getMetrics();
-  return NextResponse.json(metrics);
-}
